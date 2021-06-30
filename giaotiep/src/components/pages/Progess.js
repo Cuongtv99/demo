b486ec611d1b7a7ac0bd49060d29730217000977
Cuongtv99 <badlegolas.9g@gmail.com>
2021-06-30 14:18:13 +0700

    update

--- giaotiep/src/components/pages/Progess.js
+++ giaotiep/src/components/pages/Progess.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ProgessItem from "../ProgessItem";
 
 function Progess() {
   return (
     <div>
+      <Link to="/test">
+        <i className="fa fa-arrow-left" style={{ cursor: "pointer" }}></i>
+      </Link>
       <h1 className="progess">Tiến độ học tập</h1>
       <div className="dropdown ms-3">
         <button
@@ -34,7 +36,52 @@ function Progess() {
           </li>
         </ul>
       </div>
-      <ProgessItem />
+      <form className="row g-3" style={{ padding: "80px 80px" }}>
+        <div className="col-12">
+          <i className="progess-icon fa fa-book"></i>
+          <label htmlFor="inputEmail4" className="form-label">
+            Hoàn thành 50% nội dung khoá học
+          </label>
+
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: "50%" }}
+              aria-valuenow={50}
+              aria-valuemin={0}
+              aria-valuemax={100}
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <i className="fa fa-crosshairs"></i>
+          <label htmlFor="inputCity" className="form-label">
+            % chính xác <br />
+            100
+          </label>
+        </div>
+        <div className="col-md-4">
+          <i className="fa fa-question"></i>
+          <label htmlFor="inputState" className="form-label">
+            Đã làm <br />
+            12 câu
+          </label>
+        </div>
+        <div className="col-md-6">
+          <i className="fa fa-tachometer-alt"></i>
+          <label htmlFor="inputCity" className="form-label">
+            Tốc độ <br /> 3 câu/ phút
+          </label>
+        </div>
+        <div className="col-md-4">
+          <i className="fab fa-font-awesome-flag"></i>
+          <label htmlFor="inputState" className="form-label">
+            Hoàn thành
+            <br />0 bài
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
