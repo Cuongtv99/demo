c5c331bf49fcd4c662c0cf1351a0046925f07650
Cuongtv99 <badlegolas.9g@gmail.com>
2021-10-12 12:42:39 +0700

    update port

--- New folder/BEDemo/index.js
+++ New folder/BEDemo/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("Helloworld");
 });
-
-app.listen(port, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
